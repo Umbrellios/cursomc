e67768cf6d7120a7be9fa29872910071da86a28f
unknown <braulio926@gmail.com>
2019-04-20 17:42:51 -0300

    Ajuste no endpoint /categorias/{id}

--- src/main/java/com/umbrellios/cursomc/services/CategoriaService.java
+++ src/main/java/com/umbrellios/cursomc/services/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.umbrellios.cursomc.domain.Categoria;
 import com.umbrellios.cursomc.repositories.CategoriaRepository;
+import com.umbrellios.cursomc.services.exception.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+
 		
 		
 		
