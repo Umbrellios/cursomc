38eeabc00724062cd8ce67318496a762c6a7cb55
unknown <braulio926@gmail.com>
2019-04-24 09:13:22 -0300

    Endoint /clientes/{id} disponivel

--- src/main/java/com/umbrellios/cursomc/CursomcApplication.java
+++ src/main/java/com/umbrellios/cursomc/CursomcApplication.java
@@ -98,6 +98,8 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		
 		
+		
+		
 				
 	}
 
---
+++ src/main/java/com/umbrellios/cursomc/resources/ClienteResource.java
@@ -0,0 +1,27 @@
+package com.umbrellios.cursomc.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.umbrellios.cursomc.domain.Cliente;
+import com.umbrellios.cursomc.services.ClienteService;
+
+
+@RestController	
+@RequestMapping(value="/clientes")
+public class ClienteResource {
+	
+	@Autowired
+	private ClienteService service;
+	
+	@RequestMapping(value="/{id}", method=RequestMethod.GET)
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Cliente obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
